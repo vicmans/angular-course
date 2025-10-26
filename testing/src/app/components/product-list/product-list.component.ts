3a6b41ca4ac4095028c8fc5392a72922f54e4e0d
Victor <vsanchezl94@gmail.com>
2025-10-26 18:11:25 -0400

    chore: update angular version testing app

--- testing/src/app/components/product-list/product-list.component.ts
+++ testing/src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,6 @@ import { ProductCardComponent } from "../product-card/product-card.component";
 
 @Component({
   selector: 'app-product-list',
-  standalone: true,
   imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
 })
